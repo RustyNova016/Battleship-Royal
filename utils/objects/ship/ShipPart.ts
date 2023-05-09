218a7259f1ce8c3f3b8a96056be0208c9023688b
RustyNova <rusty.nova.jsb@gmail.com>
2023-05-09 14:37:01 +0200

    Ship Placement rewrite 2

--- utils/objects/ship/ShipPart.ts
+++ utils/objects/ship/ShipPart.ts
@@ -1,29 +1,56 @@
 import {GameBoardCell} from "@/utils/objects/GameBoardCell";
 import {PlacedShip} from "@/utils/objects/ship/PlacedShip/PlacedShip";
+import {Ok, Result} from "@rustynova/monads";
 
 /** Part of the ship. AKA the portion of a ship at a given cell */
 export class ShipPart {
     /** The cell in which the part reside */
-    public cell: GameBoardCell;
-    //TODO: Make sprites where this actually matter
-    connections: ["up"?, "right"?, "down"?, "left"?];
-    public ship: PlacedShip;
+    private _cell: GameBoardCell;
+    private readonly _ship: PlacedShip;
 
-    constructor(ship: PlacedShip, cell: GameBoardCell, connections: ["up"?, "right"?, "down"?, "left"?]) {
-        this.ship = ship
-        this.ship.parts.push(this)
-        this.cell = cell;
-        this.cell.shipPart = this
-        this.connections = connections;
+    private constructor(ship: PlacedShip, cell: GameBoardCell) {
+        this._ship = ship;
+        this._cell = cell;
     }
 
-    private _destroyed = false
-
-    get destroyed(): boolean {
-        return this._destroyed;
+    get cell(): GameBoardCell {
+        return this._cell;
     }
 
+    get ship(): PlacedShip {
+        return this._ship;
+    }
+
+    private _isDestroyed = false;
+
+    /** Return true if the part is destroyed */
+    get isDestroyed(): boolean {
+        return this._isDestroyed;
+    }
+
+    /** Return true if the part is placed on the board */
+    public get isPlaced() {return this._cell.shipPart.isSomeAnd(cellPart => cellPart.eq(this));}
+
+    /** Create a new ship part */
+    public static create(ship: PlacedShip, cell: GameBoardCell): ShipPart {
+        return new this(ship, cell);
+    }
+
+    public canBePlaced() {return this._cell.canPutShipPart(this);}
+
+    /** Destroy this part of the ship */
     public destroy() {
-        this._destroyed = true;
+        this._isDestroyed = true;
+    }
+
+    /** Check for equality */
+    public eq(other: ShipPart) {
+        return this._cell.eq(other._cell)
+            && this._ship.eq(other._ship);
+    }
+
+    public place(): Result<ShipPart, Error> {
+        if (this.isPlaced) {return Ok(this);}
+        return this._cell.setShipPart(this).replaceOk(this);
     }
 }
